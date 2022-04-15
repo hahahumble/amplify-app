b06edcd4f4179367e991d5dcbd21e73548a2ac69
hahahumble <hexichn@outlook.com>
2022-04-15 14:11:50 +0800

    mui

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
 import Todo from "./Todo";
+import MyCard from "./MyCard";
 Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
   return (
     <>
       <Todo></Todo>
+      <MyCard></MyCard>
       <h1>Hello {user.username}</h1>
       <button onClick={signOut}>Sign out</button>
     </>

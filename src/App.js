7e39b8c83c48b38c2b23106d4dd44d4adcb004cd
hahahumble <hexichn@outlook.com>
2022-04-15 13:48:52 +0800

    graphql

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
+import Todo from "./Todo";
 Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
   return (
     <>
+      <Todo></Todo>
       <h1>Hello {user.username}</h1>
       <button onClick={signOut}>Sign out</button>
     </>
